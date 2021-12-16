2831a3f143f3b0c2e06acf671d86f52c5b2d024f
harpreet-1998 <harpreetrangi704@gmail.com>
2021-12-16 13:22:07 +0530

    callback

--- index.js
+++ index.js
@@ -1,27 +1,73 @@
-const john ={
-    name: "john wade",
-    yearOfBirth:1988,
-    country: "England"
+// const john ={
+//     name: "john wade",
+//     yearOfBirth:1988,
+//     country: "England"
+// }
+// const rimple={
+// name: "rimple",
+// yearOfBirth:1999,
+// country: "India"
+// }
+
+
+// const getDetails= function(currentYear){
+//     console.log(this.name);
+//     console.log(currentYear-this.yearOfBirth+" years Old");
+//     console.log(this.country);
+// }
+
+// const btn= document.querySelector(".btn");
+// btn.addEventListener("click",getDetails.bind(john,2021));
+// console.log(getDetails.bind(john));
+
+// getDetails.bind(john,2021)();
+
+// getDetails.call(rimple,2021);
+
+// getDetails.apply(rimple, [2021]);
+
+
+function displayCallback(res){
+console.log(JSON.parse(res));
 }
-const rimple={
-name: "rimple",
-yearOfBirth:1999,
-country: "India"
+
+function getVCDetails(calback){
+    let request= new XMLHttpRequest();
+    request.open('GET', "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=148023&date=16-12-2021");
+    request.onload=function(){
+      if(this.response){
+          console.log('hello');
+          calback(this.response);
+      }else{
+          calback("Error"+request.status);
+      }
+    }
+    request.send();
+
 }
 
+getVCDetails(displayCallback);
 
-const getDetails= function(currentYear){
-    console.log(this.name);
-    console.log(currentYear-this.yearOfBirth+" years Old");
-    console.log(this.country);
-}
 
-const btn= document.querySelector(".btn");
-btn.addEventListener("click",getDetails.bind(john,2021));
-console.log(getDetails.bind(john));
+const myPromise= new Promise((reslove,reject)=>{
+    let request= new XMLHttpRequest();
+    request.open('GET', "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=148023&date=16-12-2021");
+    request.onload=function(){
+      if(this.response){
+          
+          reslove(this.response);
+      }else{
+           reject("Error"+request.status);
+      }
+    }
+    request.send();
+});
 
-getDetails.bind(john,2021)();
 
-getDetails.call(rimple,2021);
-
-getDetails.apply(rimple, [2021]);
+myPromise.then(
+    (value)=>{ displayCallback(value);
+    },
+    (error)=>{
+        displayCallback(error)
+    }
+)
