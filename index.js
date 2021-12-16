bab6158892d8444d5b482cb43284fa194f4ac6eb
harpreet-1998 <harpreetrangi704@gmail.com>
2021-12-16 11:18:29 +0530

    bind and call example

---
+++ index.js
@@ -0,0 +1,25 @@
+const john ={
+    name: "john wade",
+    yearOfBirth:1988,
+    country: "England"
+}
+const rimple={
+name: "rimple",
+yearOfBirth:1999,
+country: "India"
+}
+
+
+const getDetails= function(currentYear){
+    console.log(this.name);
+    console.log(currentYear-this.yearOfBirth+" years Old");
+    console.log(this.country);
+}
+
+const btn= document.querySelector(".btn");
+btn.addEventListener("click",getDetails.bind(john,2021));
+
+
+getDetails.call(rimple,2021);
+
+getDetails.apply(rimple, [2021]);
